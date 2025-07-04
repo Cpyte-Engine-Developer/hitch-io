f9d2104112f9fbde533a85f3b017210eb08f1035
arman <arman@lmde>
2025-07-04 17:33:52 +0300

    add ui/filter_widget.ui, ui/main_window.py, icons

--- __main__.py
+++ __main__.py
@@ -1,19 +1,13 @@
-# This Python file uses the following encoding: utf-8
 import sys
 
 from PySide6.QtWidgets import QApplication, QMainWindow
 
-# Important:
-# You need to run the following command to generate the ui_form.py file
-#     pyside6-uic form.ui -o ui_form.py, or
-#     pyside2-uic form.ui -o ui_form.py
 from ui_form import Ui_MainWindow
 
-class MainWindow(QMainWindow):
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+        self.setupUi(self)
 
 
 if __name__ == "__main__":
