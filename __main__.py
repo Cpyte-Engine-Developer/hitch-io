ee58cb86f7611638e31fdf9be48325f06255918c
arman <arman@lmde>
2025-07-08 15:16:34 +0300

    add navigation bar with screens
    This navigation bar replaced old code with label

--- __main__.py
+++ __main__.py
@@ -1,13 +1,22 @@
 from kivy.lang import Builder
 
 from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
+from kivymd.uix.navigationbar import MDNavigationBar, MDNavigationItem
 
 
 class MainApp(MDApp):
+    def on_switch_tabs(
+        self,
+        bar: MDNavigationBar,
+        item: MDNavigationItem,
+        item_icon: str,
+        item_text: str,
+    ):
+        self.root.ids.screen_manager.current = item_text
+        
     def build(self):
         self.theme_cls.theme_style = "Dark"
-        return Builder.load_file("src/main_window/ui/main_window.kv")
+        return Builder.load_file("src/main_window/ui/main.kv")
 
 
 MainApp().run()
