38a7bd619370108a39426b0c8ec734c7c3a64cbc
arman <arman@lmde>
2025-07-08 14:36:18 +0300

    add material design

--- __main__.py
+++ __main__.py
@@ -1,10 +1,13 @@
+from kivy.lang import Builder
+
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 
 
 class MainApp(MDApp):
     def build(self):
-        return MDLabel(text="Hello, World", halign="center")
+        self.theme_cls.theme_style = "Dark"
+        return Builder.load_file("src/main_window/ui/main_window.kv")
 
 
 MainApp().run()
