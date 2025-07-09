519e525b9b806d357598202de654aabe7e8584b3
arman <arman@lmde>
2025-07-09 14:31:26 +0300

    clear main_window.py

--- src/main_window/main_window.py
+++ src/main_window/main_window.py
@@ -1,14 +1,1 @@
-import sys
-from pathlib import Path
 
-from PySide6.QtQuick import QQuickView
-from PySide6.QtCore import QUrl
-from PySide6.QtQml import QQmlApplicationEngine
-
-
-class MainWindow(QQmlApplicationEngine):
-    def __init__(self):
-        super().__init__()
-
-        self.addImportPath(sys.path[0])
-        self.load(QUrl.fromLocalFile("src/qml/main_window.qml"))
